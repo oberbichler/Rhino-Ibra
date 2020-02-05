cbda6062ac30b13a7a7268466b233a037d9d46f0
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-02-05 13:54:52 +0100

    Add Nurbs geometry readers

---
+++ IbraImport/Readers/NurbsCurveGeometryReader.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using Rhino;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IbraImport.Readers
+{
+    class NurbsCurveGeometryReader : Reader
+    {
+        public override string Name => "NurbsCurveGeometryReader";
+
+        protected override bool IsActiveByDefault => false;
+
+        public override bool TryLoad(JObject data, Model model, RhinoDoc document)
+        {
+            if (!data.HasType(out int index, "NurbsCurveGeometry2D", "NurbsCurveGeometry3D"))
+                return false;
+
+            var attributes = GetAttributes(document, data);
+
+            if (index == 0) // 2D
+            {
+                var geometry = GetNurbsCurve2D(data);
+
+                document.Objects.AddCurve(geometry, attributes);
+            }
+            else            // 3D
+            {
+                var geometry = GetNurbsCurve3D(data);
+
+                document.Objects.AddCurve(geometry, attributes);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ IbraImport/Readers/NurbsSurfaceGeometryReader.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using Rhino;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IbraImport.Readers
+{
+    class NurbsSurfaceGeometryReader : Reader
+    {
+        public override string Name => "NurbsSurfaceGeometryReader";
+
+        protected override bool IsActiveByDefault => false;
+
+        public override bool TryLoad(JObject data, Model model, RhinoDoc document)
+        {
+            if (!data.HasType(out int index, "NurbsSurfaceGeometry2D", "NurbsSurfaceGeometry3D"))
+                return false;
+
+            var attributes = GetAttributes(document, data);
+
+            if (index == 0) // 2D
+            {
+                var geometry = GetNurbsSurface2D(data);
+
+                document.Objects.AddSurface(geometry, attributes);
+            }
+            else            // 3D
+            {
+                var geometry = GetNurbsSurface3D(data);
+
+                document.Objects.AddSurface(geometry, attributes);
+            }
+
+            return true;
+        }
+    }
+}
